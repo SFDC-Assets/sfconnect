2e241a7e4497e1ecd5797842fd196be0e6a3348c
Jesse David <jesse.p.david@gmail.com>
2021-05-03 16:00:05 -0500

    More experimentation

--- orgInit.sh
+++ orgInit.sh
@@ -2,10 +2,19 @@ echo 'Creating the new demo scratch org.'
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o sfconnect.demo
 
 export DEPLOY_NAME=sf-connect-${RANDOM}
+export DEPLOY_NAME_RAND=sf-connect-${$RANDOM}
+
+echo $DEPLOY_NAME
+echo $DEPLOY_NAME_RAND
+
 echo `Setting deployed app name to  "${DEPLOY_NAME}"`
 
 echo 'Creating the Heroku app which will provide the oData API used by this demo.'
 sfdx shane:heroku:repo:deploy -g gabesumner -r external-objects -n `"${DEPLOY_NAME}"`
+sfdx shane:heroku:repo:deploy -g gabesumner -r external-objects -n ${$DEPLOY_NAME}
+sfdx shane:heroku:repo:deploy -g gabesumner -r external-objects -n ${DEPLOY_NAME}
+
+sfdx shane:heroku:repo:deploy -g gabesumner -r external-objects -n `"${$DEPLOY_NAME}"`
 
 echo 'Updating the external data source to point to the new Heroku app.'
 sfdx shane:heroku:externalobjects -a `"${DEPLOY_NAME}"` -f force-app/main/default/dataSources/ExternalData.dataSource-meta.xml 
